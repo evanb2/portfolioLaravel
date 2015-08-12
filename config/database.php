a2643b68cd1102cb1c37973ca32bf94d7d6c1c02
Evan Butler <evanb2@mac.com>
2015-08-12 12:14:23 -0700

    Change DB connection

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', 'EfWThwghKAPyqmyjKJUE'),
+            'password'  => env('DB_PASSWORD', 'tYhEfD7dd7hYvZbQOlH8'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
