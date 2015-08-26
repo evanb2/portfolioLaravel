bb60aae6335afe7e46bbe527409b642726641880
Evan Butler <evanb2@mac.com>
2015-08-26 00:33:58 -0700

    Better align contact-imgs

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,20 +4,20 @@
 
     <div class="row">
         <div class="col-md-6 col-md-offset-3" id="contact-content">
-            <div class="row social">
-                <div class="col-sm-4">
+            <div class="row">
+                <div class="col-sm-4 social">
                     <a href="https://facebook.com/evan.butler.31">
-                        <img src="/img/contact/facebook.jpeg" alt="Facebook" width="100%" class="img-responsive"/>
+                        <img src="/img/contact/facebook.jpeg" alt="Facebook" width="100%" class="img-responsive contact-imgs"/>
                     </a>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 social">
                     <a href="https://github.com/evanb2">
-                        <img src="/img/github.jpeg" alt="GitHub" width="100%" class="img-responsive"/>
+                        <img src="/img/github.jpeg" alt="GitHub" width="100%" class="img-responsive contact-imgs"/>
                     </a>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 social">
                     <a href="https://instagram.com/manintheglass">
-                        <img src="/img/contact/instagram.jpeg" alt="Instagram" width="100%" class="img-responsive"/>
+                        <img src="/img/contact/instagram.jpeg" alt="Instagram" width="100%" class="img-responsive contact-imgs"/>
                     </a>
                 </div>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,19 +11,19 @@
     </div>
 
     <div class="row" id="contact-content">
-        <div class="col-sm-4 social-col">
+        <div class="col-sm-4 social">
             <a href="https://facebook.com/evan.butler.31">
-                <img src="/img/contact/facebook.jpeg" alt="Facebook" width="100%" class="social img-responsive"/>
+                <img src="/img/contact/facebook.jpeg" alt="Facebook" width="100%" class="img-responsive contact-imgs"/>
             </a>
         </div>
-        <div class="col-sm-4 social-col">
+        <div class="col-sm-4 social">
             <a href="https://github.com/evanb2">
-                <img src="/img/github.jpeg" alt="GitHub" width="100%" class="social img-responsive"/>
+                <img src="/img/github.jpeg" alt="GitHub" width="100%" class="img-responsive contact-imgs"/>
             </a>
         </div>
-        <div class="col-sm-4 social-col">
+        <div class="col-sm-4 social">
             <a href="https://instagram.com/manintheglass">
-                <img src="/img/contact/instagram.jpeg" alt="Instagram" width="100%" class="social img-responsive" id="instagram"/>
+                <img src="/img/contact/instagram.jpeg" alt="Instagram" width="100%" class="img-responsive contact-imgs"/>
             </a>
         </div>
     </div>
