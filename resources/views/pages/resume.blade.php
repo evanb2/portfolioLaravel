fd94a7ab4abd43fe67a3d780b802ac2eeb7de364
Evan Butler <evanb2@mac.com>
2015-08-26 00:06:10 -0700

    Put resume-img in divs for better alignment

--- resources/views/pages/resume.blade.php
+++ resources/views/pages/resume.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 
-    <img src="/img/contact/Resume.png" alt="Resume" id="resume-img" class="img-responsive" width="70%"/>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <img src="/img/contact/Resume.png" alt="Resume" id="resume-img" class="img-responsive" width="100%"/>
+        </div>
+    </div>
 
 @stop
