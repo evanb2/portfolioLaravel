7ce098da23e21b84228e56bcb6edfd597864ab09
Evan Butler <evanb2@mac.com>
2015-09-03 10:02:21 -0700

    Change social icon sizing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,17 +13,17 @@
     <div class="row" id="contact-content">
         <div class="col-sm-4 social">
             <a href="https://facebook.com/evan.butler.31">
-                <img src="/img/contact/facebook.jpeg" alt="Facebook" width="100%" class="img-responsive contact-imgs"/>
+                <img src="/img/contact/facebook.jpeg" alt="Facebook" width="50%" class="img-responsive contact-imgs"/>
             </a>
         </div>
         <div class="col-sm-4 social">
             <a href="https://github.com/evanb2">
-                <img src="/img/github.jpeg" alt="GitHub" width="100%" class="img-responsive contact-imgs"/>
+                <img src="/img/github.jpeg" alt="GitHub" width="50%" class="img-responsive contact-imgs"/>
             </a>
         </div>
         <div class="col-sm-4 social">
             <a href="https://instagram.com/manintheglass">
-                <img src="/img/contact/instagram.jpeg" alt="Instagram" width="100%" class="img-responsive contact-imgs"/>
+                <img src="/img/contact/instagram.jpeg" alt="Instagram" width="50%" class="img-responsive contact-imgs"/>
             </a>
         </div>
     </div>
