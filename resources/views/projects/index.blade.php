4f26dee2957e5a85c55d0318a9ce1158d790a159
Evan Butler <evanb2@mac.com>
2015-09-04 23:43:37 -0700

    Center project links

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,13 +5,13 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3" id="projects-index-content">
             <div class="row project-links">
-                <div class="col-sm-4">
+                <div class="col-sm-4 projects">
                     <a href="https://epifoodus.herokuapp.com">Epifoodus</a>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 projects">
                     <a href="http://titlecase.site44.com">MakeTitle</a>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 projects">
                     <a href="http://dev-adopt-an-epicoder.pantheon.io">Adopt-a-Coder</a>
                 </div>
             </div>
