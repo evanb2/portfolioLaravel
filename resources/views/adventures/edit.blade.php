75f510491c7a994c377222c7dbd09e26be0613da
Evan Butler <evanb2@mac.com>
2015-10-17 14:15:34 -0700

    Move adventure form to _form partial

--- resources/views/adventures/edit.blade.php
+++ resources/views/adventures/edit.blade.php
@@ -5,50 +5,7 @@
     <h2>Edit: {!! $adventure->trail_name !!}</h2>
 
     {!! Form::model($adventure, ['method' => 'PATCH', 'action' => ['AdventuresController@update', $adventure->id]]) !!}
-        <div class="form-group">
-            {!! Form::label('trail_name', 'Trail Name: ') !!}
-            {!! Form::text('trail_name', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('rating', 'Rating: ') !!}
-            {!! Form::input('number', 'rating', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('difficulty', 'Difficulty: ') !!}
-            {!! Form::input('number', 'difficulty', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('directions', 'Directions: ') !!}
-            {!! Form::textarea('directions', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('description', 'Description: ') !!}
-            {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('length', 'Length (mi): ') !!}
-            {!! Form::input('number', 'length', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('is_loop', 'Loop: ') !!}
-            {!! Form::input('number', 'is_loop', null, ['class' => 'form-control']) !!}
-        </div>
-
-        <div class="form-group">
-            {!! Form::label('hiked_on', 'Date Hiked: ') !!}
-            {!! Form::input('date', 'hiked_on', date('Y-m-d'), ['class' => 'form-control']) !!}
-        </div>
-
-        <!-- Add Adventure Form Input -->
-        <div class="form-group">
-            {!! Form::submit('Update Adventure', ['class' => 'btn btn-primary form-control']) !!}
-        </div>
+        @include('adventures._form', ['submitBtnText' => 'Update Adventure'])
     {!! Form::close() !!}
 
     @include('errors.list')
