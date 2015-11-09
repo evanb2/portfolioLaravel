be29398728095134a2d46083ae0579940de92c38
Evan Butler <evanb2@mac.com>
2015-11-09 14:32:44 -0800

    Add foreach loop to modal display

--- resources/views/adventures/index.blade.php
+++ resources/views/adventures/index.blade.php
@@ -9,10 +9,12 @@
         <hr>
 
         @foreach ($adventures as $adventure)
-                <!-- Button trigger modal -->
+            <!-- Button trigger modal -->
+            <ol>
                 <button type="button" class="btn btn-primary btn-lg modal-buttons" data-toggle="modal" data-target="#{{ $adventure->id }}">
                     {{ $adventure->trail_name }}
                 </button>
+            </ol>
         @endforeach
 
         <hr>
@@ -21,33 +23,35 @@
     </div>
 
     <div class="col-md-8">
-        <!-- Modal -->
-        <div class="modal fade" id="{{ $adventure->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title" id="myModalLabel">{{ $adventure->trail_name }}</h4>
-                    </div>
-                    <div class="modal-body">
-                        <ul>
-                            <li>Trail Name: {{ $adventure->trail_name }}</li>
-                            <li>Difficulty: {{ $adventure->difficulty }}</li>
-                            <li>Rating: {{ $adventure->rating }}</li>
-                            <li>Description: {{ $adventure->description }}</li>
-                            <li>Directions: {{ $adventure->directions }}</li>
-                            <li>Length: {{ $adventure->length }}</li>
-                            <li>Loop: {{ $adventure->is_loop }}</li>
-                            <li>Date Hiked: {{ $adventure->hiked_on->diffForHumans() }}</li>
-                        </ul>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+        @foreach ($adventures as $adventure)
+            <!-- Modal -->
+            <div class="modal fade" id="{{ $adventure->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="myModalLabel">{{ $adventure->trail_name }}</h4>
+                        </div>
+                        <div class="modal-body">
+                            <ul>
+                                <li>Trail Name: {{ $adventure->trail_name }}</li>
+                                <li>Difficulty: {{ $adventure->difficulty }}</li>
+                                <li>Rating: {{ $adventure->rating }}</li>
+                                <li>Description: {{ $adventure->description }}</li>
+                                <li>Directions: {{ $adventure->directions }}</li>
+                                <li>Length: {{ $adventure->length }}</li>
+                                <li>Loop: {{ $adventure->is_loop }}</li>
+                                <li>Date Hiked: {{ $adventure->hiked_on->diffForHumans() }}</li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div> <!-- /.modal -->
+            </div> <!-- /.modal -->
+        @endforeach
     </div>
 </div>
 
