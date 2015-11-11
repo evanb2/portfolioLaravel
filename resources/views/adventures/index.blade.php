d6a0098791e03e6aa1dfc5662fb0f9407a880bfd
Evan Butler <evanb2@mac.com>
2015-11-11 10:34:42 -0800

    Add edit btn to modal window

--- resources/views/adventures/index.blade.php
+++ resources/views/adventures/index.blade.php
@@ -47,7 +47,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                            <a href="/adventures/{{ $adventure->id }}/edit"><button type="button" class="btn btn-primary">Edit</button></a>
                         </div>
                     </div>
                 </div>
