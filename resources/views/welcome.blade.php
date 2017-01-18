d81763279b9b2834e1fe9e5274930e681bfa89b7
Evan Butler <evanbutler@FM-evabut-Mac.local>
2017-01-17 19:46:55 -0800

    rm Projects page, rm FB link, change prof_pic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,16 @@
     <div class="row">
         <div class="jumbotron col-lg-8 col-lg-offset-2">
             <h1 id="name">evan butler</h1>
-            <h4>Junior Developer</h4>
-            <img id="prof-pic" src="/img/prof_pic3.jpg" alt="Me at the Painted Hills with my Alaskan Husky Lior" width="50%"/>
+            <h4>Full Stack Developer</h4>
+            <img id="prof-pic" src="/img/prof_pic.jpg" alt="Me at the Painted Hills with my Alaskan Husky Lior" width="50%"/>
         </div>
     </div>
 
     <div class="row" id="contact-content">
         <!-- <div class="col-sm-4 social"> -->
-            <a href="https://facebook.com/evan.butler.31">
-                <img src="/img/contact/facebook.jpeg" alt="Facebook" width="50%" class="img-responsive contact-imgs"/>
-            </a>
+            {{--<a href="https://facebook.com/evan.butler.31">--}}
+                {{--<img src="/img/contact/facebook.jpeg" alt="Facebook" width="50%" class="img-responsive contact-imgs"/>--}}
+            {{--</a>--}}
         <!-- </div> -->
         <!-- <div class="col-sm-4 social"> -->
             <a href="https://github.com/evanb2">
