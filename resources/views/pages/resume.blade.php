1ec2ac4974b8c9efe9630e5a34838a910cb906ca
Evan Butler <ebutler@fuelmedical.com>
2017-04-12 21:43:14 -0700

    changes resume filetype

--- resources/views/pages/resume.blade.php
+++ resources/views/pages/resume.blade.php
@@ -4,7 +4,7 @@
 
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <img src="/img/contact/Resume.png" alt="Resume" id="resume-img" class="img-responsive" width="100%"/>
+            <img src="/img/contact/Resume.jpeg" alt="Resume" id="resume-img" class="img-responsive" width="100%"/>
         </div>
     </div>
 
